665d764fa0aef9da81b214a74a079ad450a16809
Manish R <manishraja2505@gmail.com>
2025-09-26 19:35:03 +0530

    minor change

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,11 +1,13 @@
 // tools/index.js
 import { getRedditPosts } from "./redditTools.js";
 import { getCurrentWeather } from "./weatherTool.js";
-import { getTrafficConditions } from "./trafficTools.js";
+import { getTrafficConditions } from "./trafficTools.js"; 
+import { getLocationTool } from "./locationTool.js" // detail of event along with route
 
 // This array contains every tool the agent has access to.
 // To add a new tool, just import it and add it to this list.
 export const allTools = [getRedditPosts,
     getCurrentWeather,
-    getTrafficConditions
+    getTrafficConditions,
+    getLocationTool
 ];
